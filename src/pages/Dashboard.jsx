caf0cc8772ac462bd91d3949619c4a98cf5dc203
Chameli <Chameli@example.com>
2022-08-16 09:02:10 +0530

    edit dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,16 +1,4 @@
 import React from 'react';
-import { BsCurrencyDollar } from 'react-icons/bs';
-import { GoPrimitiveDot } from 'react-icons/go';
-import { IoIosMore } from 'react-icons/io';
-import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
-
-import { Pie, Button, Bar} from '../components';
-import { earningData, medicalproBranding, recentTransactions, weeklyStats, dropdownData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
-import { useStateContext } from '../contexts/ContextProvider';
-
-
-import hs2 from '../data/hs2.jpg';
-
 
 const Dashboard = () => {
   return (
@@ -25,26 +13,11 @@ const Dashboard = () => {
       <div className="flex gap-10 flex-wrap justify-center">
         <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780  ">
           <div className="flex justify-between">
-            <p className="font-semibold text-xl">Revenue Updates</p>
-
-            <div className="flex items-center gap-4">
-                <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
-                  <span>
-                    <GoPrimitiveDot />
-                  </span>
-                  <span>Expense</span>
-                </p>
-                <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
-                  <span>
-                    <GoPrimitiveDot />
-                  </span>
-                  <span>Budget</span>
-                </p>
-            </div>    
+            <p className="font-semibold text-xl">Welcome to the Dashboard</p>  
           </div>
-
         </div>
       </div>
+      
     </div>
   )
 }
