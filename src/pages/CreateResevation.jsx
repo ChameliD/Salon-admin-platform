d94c2a2b3fae219217046a041a0100cb24f14f61
Chameli <Chameli@example.com>
2022-08-16 12:54:12 +0530

    install emailjs

--- src/pages/CreateResevation.jsx
+++ src/pages/CreateResevation.jsx
@@ -184,7 +184,7 @@ const CreateResevation = () => {
             <button 
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
             type="submit" >
-              Create Client
+              Create Resevation
             </button>
           
           </div>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,22 +4,29 @@ const Dashboard = () => {
   return (
     <div className="mt-12">
       <div className="flex flex-wrap lg:flex-nowrap justify-center ">
-        
-        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center"> 
           
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center"> 
+        
+        <div className="flex gap-10 flex-wrap justify-center">
+          <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780  ">
+            <div className="flex justify-between">
+              <p className="font-semibold text-xl">Welcome to the Dashboard</p>  
+            </div>
         </div>
-      </div>
-
-      <div className="flex gap-10 flex-wrap justify-center">
-        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780  ">
-          <div className="flex justify-between">
-            <p className="font-semibold text-xl">Welcome to the Dashboard</p>  
+        <div className="flex gap-10 flex-wrap justify-center">    
+            <div className="flex justify-between">
+              <button 
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
+                type="submit" >
+                  Invite to an admin
+              </button>
+            
+            </div>
           </div>
-        </div>
+        </div> 
       </div>
-      
     </div>
-  )
-}
+  </div>
+)}
 
 export default Dashboard
